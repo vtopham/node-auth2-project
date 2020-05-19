56b74fe27835e6135ab9ffee2057a0881607a685
Victoria  Topham <topham.victoria@gmail.com>
2020-05-19 09:21:59 -0700

    got a server up and running

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: "Hi there!"})
+})
+module.exports = server;
