c5c5f5697d5d3e1480d57842cc1a682a5bf9f0db
Victoria  Topham <topham.victoria@gmail.com>
2020-05-19 09:34:56 -0700

    added credential validation for signing up

--- api/api-router.js
+++ api/api-router.js
@@ -1,12 +1,13 @@
 
 // /api
 const express = require('express')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router();
 const Users = require('./api-model')
 //Creates a user using the information sent inside the body of the request. Hash the password before saving the user to the database.
-router.post('/register', (req, res) => {
-    
+router.post('/register', validateCredentials, (req, res) => {
+    res.status(200).json({message: "Hello"})
 })
 
 //Use the credentials sent inside the body to authenticate the user. On successful login, create a new JWT with the user id as the subject and send it back to the client. If login fails, respond with the correct status code and the message: 'You shall not pass!'
@@ -23,9 +24,15 @@ router.get('/users', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({message: "error retrieving users from database", error: err})
-        })
-})
+        });
+});
 
-
+function validateCredentials(req, res, next) {
+    if(req.body && req.body.username && req.body.password) {
+        next();
+    } else {
+        res.status(400).json({message: "Please include a username and password in the body"})
+    }
+}
 
 module.exports = router;
